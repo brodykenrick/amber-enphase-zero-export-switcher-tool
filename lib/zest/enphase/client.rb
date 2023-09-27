8222a4003cb5513d0e48f1316d46d43472feb881
Brody <brody@ieee.org>
2023-09-27 11:59:00 +1000

    Tidied up logging mess

--- lib/zest/enphase/client.rb
+++ lib/zest/enphase/client.rb
@@ -15,9 +15,6 @@ module Zest
         @envoy_installer_password = envoy_installer_password
         @envoy_installer_session_id = nil
         self.get_refreshed_token()
-
-        #logger.info( "initialize:" + (@envoy_installer_session_id).to_str )
-
       end
 
       def set_current_grid_profile(name:)
@@ -26,67 +23,23 @@ module Zest
       end
 
       def get_refreshed_token()
+        #Initially this followed the Enphase documentation:
         #From: https://enphase.com/download/accessing-iq-gateway-local-apis-or-local-ui-token-based-authentication
-        logger.info('get_refreshed_token')
-
-        #response = httpTokenRefreshLogin.post(
-        #  'https://enlighten.enphaseenergy.com/login/login.json?',
-        #  form: { 'user[email]': envoy_installer_username, 'user[password]': envoy_installer_password }
-        #  )
-
+        # but tokens created that way are missing "something" (they don't work for set_profile....)
 
         response = httpTokenRefreshLogin.post(
           'https://entrez.enphaseenergy.com/login',
           form: { 'username': envoy_installer_username, 'password': envoy_installer_password }
           )
 
-        #logger.info('Response')
-        #logger.info(response.body.to_json)
-        #logger.info('YYYYYYY')
-
-
-        #temp = JSON.parse(response)
-        #logger.info( temp['session_id'] )
-
-
-        #responseTokens = httpTokenRefreshLogin.post(
-        #  'https://entrez.enphaseenergy.com/tokens',
-        #  json: {'session_id': temp['session_id'], 'serial_num': envoy_serial_number, 'username':envoy_installer_username}
-        #  )
-
-        #logger.info('ResponseTokens:')
-        #logger.info(responseTokens.body.to_json)
-
-        #@envoy_installer_session_id = responseTokens.to_str
-
-        #logger.info( @envoy_installer_session_id )
-
-
-
-
-
         #Weird - regular tokens don't give you access to the set_profile commands.... Had to go and get it another way!!!!!!
         responseTokensOther = httpTokenRefresh.post(
           'https://entrez.enphaseenergy.com/entrez_tokens',
           form: {'uncommissioned':'on', 'Site':"", 'serialNum':envoy_serial_number}
           )
-
-        #logger.info('ResponseTokensOther:')
+        #This is the text area containing the copy-and-paste code from the Entrez UI
         document = Nokogiri::HTML(responseTokensOther.body.to_s)
         @envoy_installer_session_id = document.at('textarea').text
-        #logger.info( @envoy_installer_session_id )
-
-
-
-        responseCheckJWT = httpCheckJWT.post(
-          "#{base_url}/auth/check_jwt"
-        )
-        #logger.info('responseCheckJWT:')
-        #logger.info(responseCheckJWT.body.to_json)
-        #logger.info( responseCheckJWT.headers.to_s )
-        #logger.info( responseCheckJWT.headers["set-cookie"] )
-        #logger.info( httpCheckJWT.cookies.to_s )
-
 
         response.raise_for_status
       end
@@ -118,30 +71,12 @@ module Zest
             #.with(debug: STDERR, debug_level: 2)
       end
 
-      def httpCheckJWT()
-        #logger.info( "httpCheckJWT(...@):" + @envoy_installer_session_id )
-
-        @httpCheckJWT ||=
-          HTTPX
-            .with_headers('Accept' => 'application/json')
-            .plugin(:authentication)
-            .authentication("Bearer #{@envoy_installer_session_id}")
-            .plugin(:cookies)
-            .plugin(:persistent)
-            .with(ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-            #.with(debug: STDERR, debug_level: 2)
-      end
-
       def http()
-        #logger.info( "http(...@):" + @envoy_installer_session_id )
-
         @http ||=
           HTTPX
             .with_headers('Accept' => 'application/json')
             .plugin(:authentication)
             .authentication("Bearer #{@envoy_installer_session_id}")
-            .plugin(:cookies)
-            .with_cookies(httpCheckJWT.cookies)
             .plugin(:persistent)
             .with(ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
             #.with(debug: STDERR, debug_level: 2)
